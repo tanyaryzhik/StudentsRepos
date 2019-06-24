2eb9f0551a10e020382540c9ba488fb4c50abe2e
Tanya <tanyaryzhik33@gmail.com>
2019-06-24 21:45:07 +0300

    Student class

---
+++ StudentsApp/Student.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StudentsApp
+{
+    public class Student
+    {
+        public string FirstName { get; set; }
+
+        public string LastName { get; set; }
+
+        public string Faculty { get; set; }
+
+        public int Course { get; set; }
+
+        public int Group { get; set; }
+    }
+}
